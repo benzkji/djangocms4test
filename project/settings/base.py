e109d1ab9e2a2fb7111f99ae576e3292aa9017f6
Ben Stähli <bnzk@bnzk.ch>
2023-10-09 16:16:53 +0200

    cleanup

--- project/settings/base.py
+++ project/settings/base.py
@@ -1,11 +1,7 @@
 import os
 import sys
-import subprocess
 
 import environ
-
-# just in case - know the defaults ;-)
-# import django.conf.global_settings as DEFAULT_SETTINGS
 
 # helpers and such
 PROJECT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../")
@@ -60,10 +56,6 @@ INTERNAL_IPS = (
     "127.0.0.1",
     "0.0.0.0",
 )
-MIGRATION_MODULES = {
-    "textblocks": "apps.djangocms4test.migrations_textblocks",
-    # 'image': 'apps.djangocms4test.migrations_plugins.image',
-}
 SITE_ID = env.int("SITE_ID")
 ROOT_URLCONF = "project.urls"
 # default time zone
@@ -136,35 +128,3 @@ TEMPLATES = [
         },
     },
 ]
-
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-"""
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-"""
